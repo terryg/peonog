264832aa8638326309a02e73652af4637ba844d4
Terry Lorber <tgl@rideside.net>
2013-01-10 23:49:29 -0500

    first image

--- app/routes/site.rb
+++ app/routes/site.rb
@@ -3,4 +3,21 @@ class Main
     @redis = monk_settings(:redis)
     haml :home
   end
+
+  get "/Landscapes" do
+    haml :landscapes
+  end
+
+  get "/Oysters" do
+    haml :oysters
+  end
+
+  get "/CV" do
+    haml :cv
+  end
+
+  get "/Contact" do
+    haml :contact
+  end
+
 end
