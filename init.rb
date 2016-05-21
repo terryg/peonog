a5ee2a8b0957673170f363b7fc3c8ffb2357eda6
Terry G Lorber <tgl@rideside.net>
2016-05-20 22:23:52 -0400

    DOn't try AWS if no credentials

--- init.rb
+++ init.rb
@@ -11,11 +11,13 @@ require "aws/s3"
 require "mini_magick"
 
 DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, (ENV['HEROKU_POSTGRESQL_GOLD_URL'] || "postgres://himself:password@localhost/baldur_development"))
+DataMapper.setup(:default, (ENV['HEROKU_POSTGRESQL_GOLD_URL'] || "postgres://himself:password@localhost/peonog_development"))
 
-AWS::S3::Base.establish_connection!(
-  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-)
+if not (ENV['AWS_ACCESS_KEY_ID']).blank?
+  AWS::S3::Base.establish_connection!(
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  )
+end
 
 
